e4cc8f318fa4914524403fa9c6a45c61a2c3a941
viniciuspdiogo <viniciusp.diogo@hotmail.com>
2022-06-23 22:25:04 -0300

    Adição de tela de cadastro e subscribe no graphcms e ajustes css

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o7n7ks14bl01z2ea64cabp/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization':`Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache(),
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage:{
+        blur: 'url(/src/assets/blur-background.png)'
+      },
       fontFamily:{
         sans: 'Roboto, sans-serif',
       },
